015a39201f2b44396d40b9bb631849a0e75240d9
NM-Natuva <mokshithnatuva8@gmail.com>
2025-04-11 10:00:49 +0530

    lll

--- src/pages/Men.tsx
+++ src/pages/Men.tsx
@@ -16,7 +16,7 @@ const Men: React.FC = () => {
       id: '1',
       name: 'Batman Shadow Knight T-Shirt',
       price: 29.99,
-      image: 'https://images.unsplash.com/photo-1527719327859-c6ce80353573?q=80&w=1000',
+      image: 'https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1743078788_7044700.jpg?w=480&dpr=1.5',
       category: 'Men',
       tags: ['DC Comics', 'Graphic Print']
     },
@@ -25,7 +25,7 @@ const Men: React.FC = () => {
       name: 'Spider-Man Classic Logo Tee',
       price: 24.99,
       originalPrice: 34.99,
-      image: 'https://images.unsplash.com/photo-1503342394128-c104d54dba01?q=80&w=1000',
+      image: 'https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1695459061_4692972.jpg?format=webp&w=900&dpr=1.5',
       category: 'Men',
       tags: ['Marvel', 'Oversized Fit']
     },
@@ -33,7 +33,7 @@ const Men: React.FC = () => {
       id: '4',
       name: 'Iron Man Tech Suit Tee',
       price: 32.99,
-      image: 'https://images.unsplash.com/photo-1576566588028-4147f3842f27?q=80&w=1000',
+      image: 'https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1741861875_8361423.png?format=webp&w=480&dpr=1.5',
       category: 'Men',
       tags: ['Marvel', 'Long Sleeve']
     },
@@ -42,7 +42,7 @@ const Men: React.FC = () => {
       name: 'Superman Classic Symbol Tee',
       price: 26.99,
       originalPrice: 35.99,
-      image: 'https://images.unsplash.com/photo-1551799517-eb8f03cb5e6a?q=80&w=1000',
+      image: 'https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1740579063_6627508.jpg?format=webp&w=480&dpr=1.5',
       category: 'Men',
       tags: ['DC Comics', 'Sleeveless']
     },
@@ -51,7 +51,7 @@ const Men: React.FC = () => {
       id: '9',
       name: 'Hulk Smash Edition Tee',
       price: 28.99,
-      image: 'https://images.unsplash.com/photo-1618354691373-d851c5c3a990?q=80&w=1000',
+      image: 'https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1742319137_1011186.jpg?format=webp&w=480&dpr=1.5',
       category: 'Men',
       tags: ['Marvel', 'Premium Fit']
     },
@@ -60,7 +60,7 @@ const Men: React.FC = () => {
       name: 'The Flash Lightning Bolt Shirt',
       price: 25.99,
       originalPrice: 32.99,
-      image: 'https://images.unsplash.com/photo-1561052967-61fc91e48d79?q=80&w=1000',
+      image: 'https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1686370323_2426660.jpg?format=webp&w=480&dpr=1.5',
       category: 'Men',
       tags: ['DC Comics', 'Athletic Fit']
     },
@@ -68,7 +68,7 @@ const Men: React.FC = () => {
       id: '11',
       name: 'Black Panther Wakanda Tech Tee',
       price: 34.99,
-      image: 'https://images.unsplash.com/photo-1571945153237-4929e783af4a?q=80&w=1000',
+      image: 'https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1657992255_4455089.jpg?format=webp&w=480&dpr=1.5',
       category: 'Men',
       tags: ['Marvel', 'Limited Edition']
     },
@@ -76,7 +76,7 @@ const Men: React.FC = () => {
       id: '12',
       name: 'Joker Chaos Premium T-Shirt',
       price: 39.99,
-      image: 'https://images.unsplash.com/photo-1564557287817-3785e38ec1f5?q=80&w=1000',
+      image: 'https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1685447774_9691775.jpg?format=webp&w=480&dpr=1.5',
       category: 'Men',
       tags: ['DC Comics', 'Collector Item']
     }
